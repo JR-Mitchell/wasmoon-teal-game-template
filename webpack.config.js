67b5cde929eef45cce3040e617f35ed2cc83658a
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2024-11-21 20:56:59 +0000

    feat(server): implements a simple server using lua and pegasus

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const config = {
         }),
         new CopyWebpackPlugin({
             patterns: [
-                { from: 'src/assets', to: 'assets' },
+                { from: 'src/assets/server', to: '../server' },
             ],
         }),
     ],
